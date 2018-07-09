98992c245f887c945ee6ebb43b9f5e642b18e149
wazp <wazpys@gmail.com>
2018-07-09 11:58:25 -0500

    First commit

---
+++ src/renderer/Pages/LandingPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="page">
+    <div class="top">
+      <input type="text" placeholder="What are you working on?" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'landing-page'
+  }
+</script>
+
+<style>
+  .top{
+    height:2rem;
+    background:#333;
+  }
+</style>
